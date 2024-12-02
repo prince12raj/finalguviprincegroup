9df851080ef1218194fd52f96c262e042f062a25
Prince Raj <princeraj10d429bdps@gmail.com>
2024-12-02 12:55:46 +0530

    Deleted all directories

--- voting-system/src/main/java/com/princerajgroup/votingsystem/repository/CandidateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.princerajgroup.votingsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.votingsystem.model.Candidate;
-
-public interface CandidateRepository extends JpaRepository<Candidate, Long> {
-}
