e55080fdbe852aeb3aaa08991900726de2ac9020
prince12raj <princeraj10d429bdps@gmail.com>
2025-01-05 13:04:06 +0530

    final

--- voting-system/schema.sql
+++ voting-system/schema.sql
@@ -8,7 +8,7 @@ USE voting_system;
 CREATE USER 'root'@'localhost' IDENTIFIED BY '12345';
 
 -- Grant all privileges on the voting_system database to the user
-GRANT ALL PRIVILEGES ON voting_system.* TO 'voting_user'@'localhost';
+GRANT ALL PRIVILEGES ON voting_system.* TO 'root'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
@@ -45,5 +45,12 @@ ALTER TABLE votes ADD CONSTRAINT fk_user FOREIGN KEY (userId) REFERENCES users(i
 SELECT * FROM users WHERE id = 1;
 DELETE FROM users WHERE id = 1;
 ALTER TABLE users AUTO_INCREMENT = 1;
+ALTER TABLE candidates MODIFY logo_url VARCHAR(255) DEFAULT 'https://default-url.com';
+ALTER TABLE candidates MODIFY logo_url VARCHAR(255) NULL;
+ALTER TABLE candidates MODIFY COLUMN logo_url VARCHAR(255) DEFAULT 'https://default-url.com' NULL;
+ALTER TABLE users AUTO_INCREMENT = 1;
+ALTER TABLE candidates MODIFY logoUrl VARCHAR(255) NULL;
+ALTER TABLE candidates MODIFY logoUrl VARCHAR(255) DEFAULT 'default_url_value';
+
 
 
